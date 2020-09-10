c143eaafd5152c9fd338a6ba9ee71240b63801b4
Eros Pinder Valente <eros.pv@gmail.com>
2020-09-10 17:52:08 -0300

    feat: create class SignupBusiness

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,68 +1,62 @@
-export class User{
-    constructor(
-    private id: string,
-    private name: string,
-    private email: string,
-    private nickname: string,
-    private password: string,
-    ){}
+export class User {
+	constructor(
+		private id: string,
+		private name: string,
+		private email: string,
+		private nickname: string,
+		private password: string
+	) {}
 
-    getId(){
-        return this.id;
-    }
+	getId() {
+		return this.id;
+	}
 
-    getName(){
-        return this.name
-    }
+	getName() {
+		return this.name;
+	}
 
-    getEmail(){
-        return this.email;
-    }
+	getEmail() {
+		return this.email;
+	}
 
-    getNickname(){
-        return this.nickname
-    }
+	getNickname() {
+		return this.nickname;
+	}
 
-    getPassword(){
-        return this.password;
-    }
+	getPassword() {
+		return this.password;
+	}
 
-    setId(id: string){
-        this.id = id;
-    }
+	setId(id: string) {
+		this.id = id;
+	}
 
-    setName(name: string){
-        this.name = name;
-    }
+	setName(name: string) {
+		this.name = name;
+	}
 
-    setEmail(email: string){
-        this.email = email;
-    }
+	setEmail(email: string) {
+		this.email = email;
+	}
 
-    setNickname(nickname: string){
-        this.nickname = nickname;
-    }
+	setNickname(nickname: string) {
+		this.nickname = nickname;
+	}
 
-    setPassword(password: string){
-        this.password = password;
-    }
+	setPassword(password: string) {
+		this.password = password;
+	}
 
-
-    static toUserModel(user: any): User {
-        return new User(user.id, user.name, user.email, user.nickname, user.password);
-      }
-
-
-}
-
-export interface UserInputDTO{
-    name: string;
-    email: string;
-    nickname: string
-    password: string;
-}
-
-export interface LoginInputDTO{
-    email: string;
-    password: string;
+	static toUserModel(data: any): User {
+        return (
+            data &&
+            new User(
+                data.id,
+                data.name,
+                data.email,
+                data.nickname,
+                data.password
+            )
+        ) 
+	}
 }
