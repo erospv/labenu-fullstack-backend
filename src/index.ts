fb53e3c989d747c18581bbb5599f4f5c7f3ca913
Eros Pinder Valente <eros.pv@gmail.com>
2020-09-21 20:13:15 -0300

    delete  azure deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import {AddressInfo} from "net";
 import express from "express";
-import cors from "cors"
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/musicRouter";
 
@@ -10,17 +9,15 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
 
-console.log("teste")
 
 const server = app.listen(Number(process.env.PORT) ||3003, () => {
     if (server) {
       const address = server.address() as AddressInfo;
-      console.log(`Server running on https://labesound.azurewebsites.net:${address.port}`);
+      console.log(`Server running on port ${address.port}`);
     } else {
       console.error(`Server start failed`);
     }
