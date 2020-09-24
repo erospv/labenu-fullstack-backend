fb208263bb70f3a198f88f7865ca79aabb5ec9ca
Eros Pinder Valente <eros.pv@gmail.com>
2020-09-23 23:37:36 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import {AddressInfo} from "net";
 import express from "express";
+import cors from "cors"
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/musicRouter";
 
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
